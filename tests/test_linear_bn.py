63eeb7f861e4cb58874407cac9187ed2d44a6ead
KernelA <17554646+KernelA@users.noreply.github.com>
2021-07-24 14:00:49 +0300

    Fix test

--- tests/test_linear_bn.py
+++ tests/test_linear_bn.py
@@ -10,6 +10,4 @@ def test_cls_networks(sample_batch):
     layer = LinearBN(NUM_COORDS, out_features)
     output = layer(sample_batch.x)
 
-    breakpoint()
-
     assert output.shape == sample_batch.x.shape[:2] + (out_features,)
