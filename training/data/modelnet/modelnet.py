a5547bb06d78ec8932017cec9049caeb88384f10
KernelA <17554646+KernelA@users.noreply.github.com>
2023-01-29 15:05:43 +0300

    Add pyfilesystem2 as abstarction layer

--- training/data/modelnet/modelnet.py
+++ training/data/modelnet/modelnet.py
@@ -1,11 +1,13 @@
 from typing import Dict, List
 import os
-import zipfile
 import pathlib
 import logging
 import shutil
 
+import fs
+from fs.base import FS
 import torch
+from tqdm.auto import tqdm
 from torch_geometric import loader
 from torch_geometric import data
 import trimesh
@@ -16,7 +18,7 @@ from .modelnet_info import MODELNET_CLASSES, ModelNetType
 from ..dataloader_settings import LoadSettings
 
 
-def _process_data(path: str, out_path: str, pre_transform, pre_filter, class_mapping: dict):
+def _process_data(path: str, out_path: str, pre_transform, pre_filter, class_mapping: dict, index: int):
     loaded_data = trimesh.load(path)
     class_name = pathlib.Path(path).parent.parent.name
 
@@ -35,7 +37,7 @@ def _process_data(path: str, out_path: str, pre_transform, pre_filter, class_map
 
         torch.save(new_mesh_data, out_path)
     except RuntimeError:
-        return os.path.basename(out_path)
+        return index
 
     return None
 
@@ -44,7 +46,8 @@ class ModelNet(data.Dataset):
     SPLIT_TYPES = ("train", "test")
 
     def __init__(self,
-                 path_to_zip: str,
+                 file_loc: str,
+                 protocol: str,
                  data_root: str,
                  split_type: str,
                  class_names: List[str],
@@ -57,13 +60,15 @@ class ModelNet(data.Dataset):
         self.split_type = split_type
         self.class_names = class_names
 
-        self._path_to_zip = path_to_zip
-        self.n_jobs = n_jobs
+        file_url = f"{protocol}{file_loc}"
 
-        with zipfile.ZipFile(self._path_to_zip, "r") as archive:
-            zip_names = self._get_zip_names(archive)
-            self._processed_file_names = list(
-                f"mesh_{self.split_type}_{i}.pt" for i in range(len(zip_names)))
+        with fs.open_fs(file_url, writeable=False) as filesystem:
+            self._raw_fs_paths = self._get_mesh_names(filesystem)
+
+        self._file_url = file_url
+        self.n_jobs = n_jobs
+        self._processed_file_names = [os.path.join(
+            self.split_type, str(pathlib.Path(abs_path.removeprefix("/")).with_suffix(".pt"))) for abs_path in self._raw_fs_paths]
 
         super().__init__(root=data_root,
                          transform=transform,
@@ -84,55 +89,63 @@ class ModelNet(data.Dataset):
     def get_class_mapping(self) -> Dict[str, int]:
         return {class_name: i for i, class_name in enumerate(self.class_names)}
 
-    def _get_zip_names(self, zip_archive) -> List[str]:
-        zip_files = zip_archive.infolist()
-        filtered_zip_files = []
+    def _get_mesh_names(self, filesystem: FS) -> List[str]:
+        filtered_files = []
 
-        for zip_file in zip_files:
-            if zip_file.is_dir() or zip_file.filename.startswith("__MACOSX"):
+        for file_path in filesystem.walk.files(exclude_dirs=["__MACOSX*"]):
+            if file_path.startswith("__MACOSX"):
                 continue
 
-            full_path = pathlib.Path(zip_file.filename)
+            full_path = pathlib.Path(file_path)
 
             if full_path.parent.name == self.split_type and full_path.suffix == ".off":
-                filtered_zip_files.append(zip_file.filename)
+                filtered_files.append(file_path)
 
-        return filtered_zip_files
+        return filtered_files
 
     def process(self):
-        self.logger.info("Open %s", self._path_to_zip)
+        self.logger.info("Open %s.", self._file_url)
 
-        with zipfile.ZipFile(self._path_to_zip, "r") as zip_archive:
-            zip_file_names = self._get_zip_names(zip_archive)
-            zip_archive.extractall(self.raw_dir, zip_file_names)
+        temp_paths = []
+
+        with fs.open_fs(self._file_url, writeable=False) as filesystem:
+            for file_path in tqdm(self._raw_fs_paths, desc="Copy files"):
+                out_file = os.path.join(self.raw_dir, file_path.removeprefix("/"))
+
+                os.makedirs(os.path.dirname(out_file), exist_ok=True)
+
+                with open(out_file, "wb") as tmp_fie:
+                    filesystem.download(file_path, tmp_fie)
+                    temp_paths.append(out_file)
 
         try:
-            paths = [os.path.join(self.raw_dir, path) for path in zip_file_names]
+            for path in self.processed_paths:
+                os.makedirs(os.path.dirname(path), exist_ok=True)
 
-            deleted_paths: list = Parallel(n_jobs=self.n_jobs, prefer="processes", verbose=1)(
+            delete_indices: list = Parallel(n_jobs=self.n_jobs, prefer="processes", verbose=1)(
                 delayed(_process_data)(
                     src_path,
                     out_path,
                     self.pre_transform,
                     self.pre_filter,
-                    self.get_class_mapping()
-                ) for src_path, out_path in zip(paths, self.processed_paths)
+                    self.get_class_mapping(),
+                    i
+                ) for i, (src_path, out_path) in enumerate(
+                    zip(temp_paths, self.processed_paths)
+                )
             )
         finally:
             shutil.rmtree(self.raw_dir)
 
-        num_unprocessed = 0
-
-        for del_path in deleted_paths:
-            if del_path is None:
-                continue
-            num_unprocessed += 1
-            self._processed_file_names.remove(del_path)
+        delete_indices = set(filter(lambda x: x is not None, delete_indices))
+        num_unprocessed = len(delete_indices)
+        self._processed_file_names = [path for i, path in enumerate(
+            self._processed_file_names) if i not in delete_indices]
 
         if num_unprocessed > 0:
             self.logger.exception(
                 "Unexpected error in pre_transform or transforms. Remove %d files from data", num_unprocessed)
-        self.logger.info("Processed %d", len(zip_file_names) - num_unprocessed)
+        self.logger.info("Processed %d", len(self._processed_file_names))
 
     def len(self):
         return len(self.processed_file_names)
@@ -143,17 +156,22 @@ class ModelNet(data.Dataset):
 
 class ModelNet40(ModelNet):
     def __init__(self,
-                 path_to_zip: str,
+                 file_loc: str,
+                 protocol: str,
                  data_root: str,
                  split_type: str,
                  transform=None,
                  pre_transform=None,
-                 pre_filter=None):
-        super().__init__(path_to_zip,
+                 pre_filter=None,
+                 n_jobs: int = -1):
+        super().__init__(file_loc,
+                         protocol,
                          data_root,
                          split_type,
                          MODELNET_CLASSES[ModelNetType.modelnet_40],
-                         transform=transform, pre_transform=pre_transform, pre_filter=pre_filter)
+                         transform=transform, pre_transform=pre_transform,
+                         pre_filter=pre_filter,
+                         n_jobs=n_jobs)
 
     def process(self):
         return super().process()
@@ -161,14 +179,16 @@ class ModelNet40(ModelNet):
 
 class ModelNet10(ModelNet):
     def __init__(self,
-                 path_to_zip: str,
+                 file_loc: str,
+                 protocol: str,
                  data_root: str,
                  split_type: str,
                  transform=None,
                  pre_transform=None,
-                 pre_filter=None):
-        super().__init__(path_to_zip, data_root, split_type, MODELNET_CLASSES[ModelNetType.modelnet_10],
-                         transform=transform, pre_transform=pre_transform, pre_filter=pre_filter)
+                 pre_filter=None,
+                 n_jobs: int = -1):
+        super().__init__(file_loc, protocol, data_root, split_type, MODELNET_CLASSES[ModelNetType.modelnet_10],
+                         transform=transform, pre_transform=pre_transform, pre_filter=pre_filter, n_jobs=n_jobs)
 
     def process(self):
         return super().process()
@@ -178,32 +198,38 @@ class ModelNetDataset(LightningDataModule):
     def __init__(self,
                  *,
                  data_root: str,
-                 path_to_zip: str,
+                 protocol: str,
+                 file_loc: str,
                  dataset_type: ModelNetType,
                  train_load_sett: LoadSettings,
-                 test_load_sett: LoadSettings):
+                 test_load_sett: LoadSettings,
+                 n_jobs: int = -1):
         super().__init__()
         self.train_dataset = None
         self.val_dataset = None
         self.dataset_type = dataset_type
         self.train_load_sett = train_load_sett
         self.test_load_sett = test_load_sett
-        self._path_to_zip = path_to_zip
+        self._file_loc = file_loc
+        self._protocol = protocol
         self._data_root = data_root
+        self._n_jobs = n_jobs
 
     def setup(self, stage):
         cls = ModelNet10 if self.dataset_type == ModelNetType.modelnet_10 else ModelNet40
 
         if stage == "fit" or stage is None:
-            self.train_dataset = cls(path_to_zip=self._path_to_zip, data_root=self._data_root,
+            self.train_dataset = cls(file_loc=self._file_loc, protocol=self._protocol, data_root=self._data_root,
                                      split_type="train",
+                                     n_jobs=self._n_jobs,
                                      transform=self.train_load_sett.transform,
                                      pre_transform=self.train_load_sett.pre_transform,
                                      pre_filter=self.train_load_sett.pre_filter)
 
         if stage == "validate" or stage is None:
-            self.val_dataset = cls(path_to_zip=self._path_to_zip, data_root=self._data_root,
+            self.val_dataset = cls(file_loc=self._file_loc, protocol=self._protocol, data_root=self._data_root,
                                    split_type="test",
+                                   n_jobs=self._n_jobs,
                                    transform=self.train_load_sett.transform,
                                    pre_transform=self.train_load_sett.pre_transform,
                                    pre_filter=self.train_load_sett.pre_filter)
