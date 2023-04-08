e0cc3828ad189a2a63c23fb2e0924193005d74bf
KernelA <17554646+KernelA@users.noreply.github.com>
2023-04-08 23:54:56 +0300

    Add FPS sampling anf preprocess partnet

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,12 +1,41 @@
-import trimesh
+import pytest
 import torch
-import numpy as np
-from torch_geometric.data import Data
+import trimesh
+from torch_geometric.data import Batch, Data
 
-from transforms.transformations import SamplePoints
+from transforms.transformations import FPS, SamplePoints
 
 
-def test_normal_tranfsorm():
+def test_fps_sampling(sample_batch: Batch):
+    data = sample_batch.get_example(0)
+
+    downsampled_point_size = data.num_nodes // 2
+
+    fps = FPS(num_points=downsampled_point_size)
+
+    new_data = fps(data)
+
+    assert new_data.num_nodes == pytest.approx(downsampled_point_size, abs=2)
+    assert new_data.num_node_features == data.num_node_features
+    assert new_data.keys == data.keys
+
+
+def test_fps_sampling_batch(sample_batch: Batch):
+    downsampled_point_size = sample_batch.get_example(0).num_nodes // 2
+
+    fps = FPS(num_points=downsampled_point_size)
+    new_data = fps(sample_batch)
+
+    for i in range(sample_batch.num_graphs):
+        new_sample = new_data.get_example(i)
+        data_sample = sample_batch.get_example(i)
+
+        assert new_sample.num_nodes == pytest.approx(downsampled_point_size, abs=2)
+        assert new_sample.num_node_features == data_sample.num_node_features
+        assert new_sample.keys == data_sample.keys
+
+
+def test_normal_transform():
     mesh_data = trimesh.load("./media/chebur.obj")
 
     data_sample = Data(pos=torch.from_numpy(mesh_data.vertices),
--- transforms/transformations.py
+++ transforms/transformations.py
@@ -1,9 +1,12 @@
-from torch_geometric.transforms import BaseTransform
-from torch_geometric.data import Data
+from typing import Union
+
+import numpy as np
 import torch
 import trimesh
+from torch_cluster import fps
+from torch_geometric.data import Batch, Data
+from torch_geometric.transforms import BaseTransform
 from trimesh import sample
-import numpy as np
 
 
 class FeaturesFromPos(BaseTransform):
@@ -27,9 +30,57 @@ class FeaturesFromNormal(BaseTransform):
             data.x = torch.cat((data.x, data.normal), dim=-1)
         else:
             data.x = data.normal.clone()
-
         del data.normal
         return data
+
+
+class FPS(BaseTransform):
+    def __init__(self, num_points: int, device: str = "cpu") -> None:
+        super().__init__()
+        self.num_points = num_points
+        self.device = device
+
+    def _copy_node_data(self, data: Data, index):
+        new_data = Data()
+
+        for key in data.keys:
+            if data[key].numel() > 1:
+                new_data[key] = data[key][index]
+            else:
+                new_data[key] = data[key]
+
+        return new_data
+
+    def __call__(self, data: Union[Data, Batch]) -> Data:
+        num_nodes = data.get_example(0).num_nodes if isinstance(data, Batch) else data.num_nodes
+
+        assert num_nodes != 0 and num_nodes is not None
+
+        if isinstance(data, Batch):
+            assert len(set(data.get_example(i).num_nodes for i in range(data.num_graphs))
+                       ) == 1, "Number of nodes must be equal between all examples"
+
+        ratio = self.num_points / num_nodes
+
+        batch = None
+
+        if isinstance(data, Batch):
+            batch = data.batch.to(self.device)
+
+        index = fps(data.pos.to(self.device), ratio=ratio, batch=batch).cpu()
+
+        if isinstance(data, Batch):
+            new_batch = batch[index]
+            new_data = Batch.from_data_list(
+                [
+                    self._copy_node_data(data.get_example(example_index), index[new_batch == example_index] - data.ptr[example_index]) for example_index in range(data.num_graphs)
+                ]
+            )
+        else:
+            new_data = Data()
+            new_data = self._copy_node_data(data, index)
+
+        return new_data
 
 
 class SamplePoints(BaseTransform):
@@ -38,6 +89,10 @@ class SamplePoints(BaseTransform):
         self.include_normals = include_normals
 
     def __call__(self, data: Data) -> Data:
+        if "y" in data:
+            if data.y.numel() > 1:
+                raise RuntimeError("Cannot apply sampling on data with multiple values of 'y'")
+
         mesh = trimesh.Trimesh(vertices=data.pos.numpy(), faces=data.face.numpy())
         samples, face_indices = sample.sample_surface(mesh, count=self.num_points)
 
@@ -54,12 +109,3 @@ class SamplePoints(BaseTransform):
             new_data.normal /= torch.linalg.norm(new_data.normal, dim=1, keepdim=True)
 
         return new_data
-
-
-class TestPointSample(BaseTransform):
-    def __init__(self, num_points: int = 1024):
-        self.point_sampler = SamplePoints(num_points=num_points)
-
-    def __call__(self, data) -> bool:
-        self.point_sampler(data)
-        return True
